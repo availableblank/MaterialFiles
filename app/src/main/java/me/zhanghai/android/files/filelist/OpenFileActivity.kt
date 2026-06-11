afce227350ce3a869baa9de1783f3a4605785f30
availableblank <yexiao105835140@gmail.com>
2026-06-11 23:54:30 +0800

    更新 OpenFileActivity.kt

--- app/src/main/java/me/zhanghai/android/files/filelist/OpenFileActivity.kt
+++ app/src/main/java/me/zhanghai/android/files/filelist/OpenFileActivity.kt
@@ -18,6 +18,8 @@ import me.zhanghai.android.files.provider.archive.isArchivePath
 import me.zhanghai.android.files.util.createViewIntent
 import me.zhanghai.android.files.util.extraPath
 import me.zhanghai.android.files.util.startActivitySafe
+import me.zhanghai.android.files.viewer.image.ImageViewerActivity
+import me.zhanghai.android.files.viewer.text.TextEditorActivity
 
 class OpenFileActivity : AppActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,14 +35,35 @@ class OpenFileActivity : AppActivity() {
     }
 
     private fun openFile(path: Path, mimeType: MimeType) {
-        if (path.isArchivePath) {
-            FileJobService.open(path, mimeType, false, this)
-        } else {
-            val intent = path.fileProviderUri.createViewIntent(mimeType)
-                .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-                .apply { extraPath = path }
-            startActivitySafe(intent)
+        when {
+            path.isArchivePath -> {
+                FileJobService.open(path, mimeType, false, this)
+            }
+            mimeType.isImageType() -> {
+                openImage(path)
+            }
+            mimeType.isTextType() -> {
+                openText(path)
+            }
+            else -> {
+                val intent = path.fileProviderUri.createViewIntent(mimeType)
+                    .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                    .apply { extraPath = path }
+                startActivitySafe(intent)
+            }
         }
+    }
+
+    private fun openImage(path: Path) {
+        val intent = Intent(this, ImageViewerActivity::class.java)
+        ImageViewerActivity.putExtras(intent, listOf(path), 0)
+        startActivity(intent)
+    }
+
+    private fun openText(path: Path) {
+        val intent = Intent(this, TextEditorActivity::class.java)
+            .apply { extraPath = path }
+        startActivity(intent)
     }
 
     companion object {
@@ -53,3 +76,20 @@ class OpenFileActivity : AppActivity() {
                 .apply { extraPath = path }
     }
 }
+
+// MIME 类型判断扩展
+private fun MimeType.isImageType(): Boolean = value.startsWith("image/")
+
+private fun MimeType.isTextType(): Boolean =
+    value.startsWith("text/") ||
+    value in listOf(
+        "application/json",
+        "application/xml",
+        "application/javascript",
+        "application/xhtml+xml",
+        "application/x-sh",
+        "application/x-shellscript",
+        "application/x-perl",
+        "application/x-python",
+        "application/x-httpd-php",
+    )
